bba022a375d52f09d5e959c3909b29f91e41655f
Davide Spada <dav.spada@outlook.com>
2024-05-28 19:47:09 +0200

    implemented sign up and modified layouts, added sign in redirect button

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,20 +1,70 @@
 "use client";
-import React from "react";
+import React , {useState} from "react";
 import { Label } from "../src/components/ui/label";
 import { Input } from "../src/components/ui/input";
 import { WavyBackground } from "../src/components/ui/wavy-background";
 import { cn } from "../src/lib/utils";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
+import Link from "next/link";
+import Swal from 'sweetalert2'
+
 
 export function SignupFormDemo() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [prontogramusername, setProntogramusername] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
+
+    if (!username || !password || !name || !prontogramusername || !address || !email) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please fill in all the fields.",
+      });
+      return;
+    }
+
+    try {
+      const response = await fetch(`http://localhost:8080/v1/signup/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password, name, prontogramusername, address, email }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to register");
+      }
+
+      const data = await response.json();
+      console.log("Registration successful", data);
+
+      Swal.fire({
+        icon: "success",
+        title: "Welcome!",
+        text: "The user "+username+" has been successfully registered!",
+        footer: '<a href="/login">Go to login page</a>'
+      });
+
+    } catch (error) {
+      console.error("Error:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Registration failed, please check your credentials or existing accounts.",
+      });
+    } finally {
+    }
   };
+
   return (
     <WavyBackground className="max-w-4xl mx-auto pb-40">
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
@@ -29,28 +79,27 @@ export function SignupFormDemo() {
         
         <LabelInputContainer className="mb-4">
           <Label htmlFor="Name">Name and Surname</Label>
-          <Input id="Name" placeholder="Mario Rossi" type="text" />
+          <Input id="Name" placeholder="Mario Rossi" type="text" onChange={(e) => setName(e.target.value)} />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="Username">Username</Label>
-          <Input id="Username" placeholder="mario_rossi" type="text" />
-        </LabelInputContainer>
-        
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="ProntogramUsername">Prontogram Username</Label>
-          <Input id="ProntogramUsername" placeholder="mariorossi10" type="text" />
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="mario.rossi@email.com" type="email" />
+          <Input id="Username" placeholder="mario_rossi" type="text" onChange={(e) => setUsername(e.target.value)}/>
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+          <Input id="password" placeholder="••••••••" type="password" onChange={(e) => setPassword(e.target.value)}/>
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email Address</Label>
+          <Input id="email" placeholder="mario.rossi@email.com" type="email" onChange={(e) => setEmail(e.target.value)} />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="ProntogramUsername">Prontogram Username</Label>
+          <Input id="ProntogramUsername" placeholder="mariorossi10" type="text" onChange={(e) => setProntogramusername(e.target.value)}/>
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="Address">Address</Label>
-          <Input id="Address" placeholder="Mura Anteo Zamboni, 7, 40126 Bologna BO" type="text" />
+          <Input id="Address" placeholder="Mura Anteo Zamboni, 7, 40126 Bologna BO" type="text" onChange={(e) => setAddress(e.target.value)} />
         </LabelInputContainer>
 
 
@@ -63,6 +112,20 @@ export function SignupFormDemo() {
         </button>
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+          Already have an account? Sign in now
+        </p>
+        <Link href="/login">
+        <button 
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
+          >
+            Login
+            <BottomGradient />
+          </button>
+        <BottomGradient />
+        </Link>
 
         {/*<div className="flex flex-col space-y-4">
           <button
