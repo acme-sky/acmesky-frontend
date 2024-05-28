1f306b59e6f1553670a52a1490048535d36506f5
Davide Spada <dav.spada@outlook.com>
2024-05-28 19:00:32 +0200

    add signup button redirect from login

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,6 +6,7 @@ import { WavyBackground } from "../src/components/ui/wavy-background";
 import { cn } from "../src/lib/utils";
 import Swal from 'sweetalert2'
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export function SignupFormDemo() {
   const [username, setUsername] = useState("");
@@ -96,6 +97,19 @@ export function SignupFormDemo() {
 
           <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
         </form>
+        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+          Don't have an account? Sign up now
+        </p>
+        <Link href="/signup">
+        <button 
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
+          >
+            Sign Up
+            <BottomGradient />
+          </button>
+        <BottomGradient />
+        </Link>
       </div>
     </WavyBackground>
   );
