bbc06375c64518136ddc248ee57bfc21e57c99d2
Davide Spada <dav.spada@outlook.com>
2024-07-05 19:28:50 +0200

    fix table when time is null

--- src/components/ui/saved_interests.tsx
+++ src/components/ui/saved_interests.tsx
@@ -17,86 +17,101 @@ import { ColumnDef } from '@tanstack/react-table';
 import { format } from 'date-fns';
 
 export const columns: ColumnDef<Interest>[] = [
-    {
-      accessorKey: "id",
-      header: "ID",
-    },
-    {
-      accessorKey: "created_at",
-      header: "Created At",
-      cell: ({ cell }) => {
-        const date = new Date(cell.getValue<string>());
-        return (
-          <div className="whitespace-nowrap overflow-hidden text-ellipsis">
-            {format(date, 'dd/MM/yyyy - HH:mm')}
-          </div>
-        );
-      }
-    },
-    {
-      accessorKey: "flight1_departure_time",
-      header: "Outward Flight Departure Time",
-      cell: ({ cell }) => {
-        const date = new Date(cell.getValue<string>());
-        return (
-          <div className="whitespace-nowrap overflow-hidden text-ellipsis">
-            {format(date, 'dd/MM/yyyy - HH:mm')}
-          </div>
-        );
-      }
-    },
-    {
-      accessorKey: "flight1_departure_airport",
-      header: "Outward Flight Departure Airport",
-    },
-    {
-      accessorKey: "flight1_arrival_time",
-      header: "Outward Flight Arrival Time",
-      cell: ({ cell }) => {
-        const date = new Date(cell.getValue<string>());
-        return (
-          <div className="whitespace-nowrap overflow-hidden text-ellipsis">
-            {format(date, 'dd/MM/yyyy - HH:mm')}
-          </div>
-        );
-      }
-    },
-    {
-      accessorKey: "flight1_arrival_airport",
-      header: "Outward Flight Arrival Airport",
-    },
-    {
-      accessorKey: "flight2_departure_time",
-      header: "Return Flight Departure Time",
-      cell: ({ cell }) => {
-        const date = new Date(cell.getValue<string>());
-        return (
-          <div className="whitespace-nowrap overflow-hidden text-ellipsis">
-            {format(date, 'dd/MM/yyyy - HH:mm')}
-          </div>
-        );
-      }
-    },
-    {
-      accessorKey: "flight2_departure_airport",
-      header: "Return Flight Departure Airport",
-    },
-    {
-      accessorKey: "flight2_arrival_time",
-      header: "Return Flight Arrival Time",
-      cell: ({ cell }) => {
-        const date = new Date(cell.getValue<string>());
-        return (
-          <div className="whitespace-nowrap overflow-hidden text-ellipsis">
-            {format(date, 'dd/MM/yyyy - HH:mm')}
-          </div>
-        );
-      }
-    },
-    {
-      accessorKey: "flight2_arrival_airport",
-      header: "Return Flight Arrival Airport",
-    },
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "created_at",
+    header: "Created At",
+    cell: ({ cell }) => {
+      const dateStr = cell.getValue<string>();
+      if (!dateStr) return null; // Return null to render nothing if dateStr is null
+
+      const date = new Date(dateStr);
+      return (
+        <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+          {format(date, 'dd/MM/yyyy - HH:mm')}
+        </div>
+      );
+    }
+  },
+  {
+    accessorKey: "flight1_departure_time",
+    header: "Outward Flight Departure Time",
+    cell: ({ cell }) => {
+      const dateStr = cell.getValue<string>();
+      if (!dateStr) return null; // Return null to render nothing if dateStr is null
+
+      const date = new Date(dateStr);
+      return (
+        <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+          {format(date, 'dd/MM/yyyy - HH:mm')}
+        </div>
+      );
+    }
+  },
+  {
+    accessorKey: "flight1_departure_airport",
+    header: "Outward Flight Departure Airport",
+  },
+  {
+    accessorKey: "flight1_arrival_time",
+    header: "Outward Flight Arrival Time",
+    cell: ({ cell }) => {
+      const dateStr = cell.getValue<string>();
+      if (!dateStr) return null; // Return null to render nothing if dateStr is null
+
+      const date = new Date(dateStr);
+      return (
+        <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+          {format(date, 'dd/MM/yyyy - HH:mm')}
+        </div>
+      );
+    }
+  },
+  {
+    accessorKey: "flight1_arrival_airport",
+    header: "Outward Flight Arrival Airport",
+  },
+  {
+    accessorKey: "flight2_departure_time",
+    header: "Return Flight Departure Time",
+    cell: ({ cell }) => {
+      const dateStr = cell.getValue<string>();
+      if (!dateStr) return null; // Return null to render nothing if dateStr is null
+
+      const date = new Date(dateStr);
+      return (
+        <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+          {format(date, 'dd/MM/yyyy - HH:mm')}
+        </div>
+      );
+    }
+  },
+  {
+    accessorKey: "flight2_departure_airport",
+    header: "Return Flight Departure Airport",
+  },
+  {
+    accessorKey: "flight2_arrival_time",
+    header: "Return Flight Arrival Time",
+    cell: ({ cell }) => {
+      const dateStr = cell.getValue<string>();
+      if (!dateStr) return null; // Return null to render nothing if dateStr is null
+
+      const date = new Date(dateStr);
+      return (
+        <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+          {format(date, 'dd/MM/yyyy - HH:mm')}
+        </div>
+      );
+    }
+  },
+  {
+    accessorKey: "flight2_arrival_airport",
+    header: "Return Flight Arrival Airport",
+  },
 ];
 
 const fetcher = (url: string, token:string | null | unknown) =>
