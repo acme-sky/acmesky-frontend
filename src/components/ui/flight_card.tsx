fa48fb496e720187ac2452e9b4ee9b4e0373158b
Davide Spada <dav.spada@outlook.com>
2024-07-06 20:34:04 +0200

    journeys cards and pages changes

--- src/components/ui/flight_card.tsx
+++ src/components/ui/flight_card.tsx
@@ -1,61 +1,52 @@
-import { BellIcon, CheckIcon } from "@radix-ui/react-icons";
+import { CheckIcon } from "@radix-ui/react-icons";
 import { cn } from "../../lib/utils";
 import { Button } from "./button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "./card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
 import { Flight_info } from "@/types";
+import { format } from "date-fns";
 
 type FlightCardProps = {
   info: Flight_info;
 } & React.ComponentProps<typeof Card>;
 
-export function FlightCard({
-  info,
-  className,
-  ...props
-}: FlightCardProps) {
+export function FlightCard({ info, className, ...props }: FlightCardProps) {
   return (
-    <Card className={cn("w-[380px]", className)} {...props}>
+    <Card className={cn("w-[480px]", className)} {...props}>
       <CardHeader>
-        <CardTitle className="flex items-center">{info.departure_airport} - {info.arrival_airport} </CardTitle>
+        <CardTitle className="flex items-center">
+          {info.departure_airport} - {info.arrival_airport}
+        </CardTitle>
       </CardHeader>
       <CardContent className="grid gap-4">
-        {/*<div className="flex items-center space-x-4 rounded-md border p-4">*/}
-          <div className="flex-1 space-y-1">
-            <p className="text-sm font-medium leading-none">
-              Airline: {info.airline}
-            </p>
-            {/*<p className="text-sm text-muted-foreground">Token: {info.token}</p>*/} 
-          {/*</div>*/}
+        <div className="space-y-2">
+          <p className="text-sm font-bold">Airline</p>
+          <p className="text-sm">{info.airline}</p>
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div>
-            <p className="text-sm font-medium leading-none">Departure</p>
-            <p className="text-sm">Airport: {info.departure_airport}</p>
-            <p className="text-sm">Time: {info.departure_time}</p>
+            <p className="text-sm font-bold">Departure</p>
+            <div className="space-y-1">
+              <p className="text-sm">Airport: {info.departure_airport}</p>
+              <p className="text-sm">
+                Day and time of departure: {format(new Date(info.departure_time), "dd/MM/yyyy - HH:mm")}
+              </p>
+            </div>
           </div>
           <div>
-            <p className="text-sm font-medium leading-none">Arrival</p>
-            <p className="text-sm">Airport: {info.arrival_airport}</p>
-            <p className="text-sm">Time: {info.arrival_time}</p>
+            <p className="text-sm font-bold">Arrival</p>
+            <div className="space-y-1">
+              <p className="text-sm">Airport: {info.arrival_airport}</p>
+              <p className="text-sm">
+                Day and time of arrival: {format(new Date(info.arrival_time), "dd/MM/yyyy - HH:mm")}
+              </p>
+            </div>
           </div>
           <div className="col-span-2">
-            <p className="text-sm font-medium leading-none">Cost</p>
+            <p className="text-sm font-bold">Cost</p>
             <p className="text-sm">${info.cost}</p>
           </div>
         </div>
       </CardContent>
-      {/*<CardFooter>
-        <Button className="w-full">
-          <CheckIcon className="mr-2 h-4 w-4" /> Confirm Booking
-        </Button>
-      </CardFooter>*/}
     </Card>
   );
 }
--- src/components/ui/journey_card.tsx
+++ src/components/ui/journey_card.tsx
@@ -1,25 +1,22 @@
+"use client";
+
 import { useState } from "react";
 import { CheckIcon, EyeOpenIcon } from "@radix-ui/react-icons";
 import { cn } from "../../lib/utils";
 import { Button } from "./button";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-} from "./card";
+import { Card, CardContent, CardHeader, CardTitle, CardFooter } from "./card";
 import { Journey_info } from "@/types";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-  DialogClose,
-} from "./dialog";
 import { FlightCard } from "./flight_card";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from "./sheet";
 
 type JourneyCardProps = {
   info: Journey_info;
@@ -30,61 +27,68 @@ export function JourneyCard({
   className,
   ...props
 }: JourneyCardProps) {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
-  const openDialog = () => setIsDialogOpen(true);
-  const closeDialog = () => setIsDialogOpen(false);
-
   return (
-    <Card className={cn("w-[380px]", className)} {...props}>
-      <CardHeader>
-        <CardTitle className="flex items-center">
-          {info.flight1.departure_airport} - {info.flight1.arrival_airport}
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="border p-4 rounded-md space-y-2">
-          <div>
-            <p className="text-sm font-medium">Flight 1</p>
-            <p className="text-xs">Departure Airport: {info.flight1.departure_airport}</p>
-            <p className="text-xs">Arrival Airport: {info.flight1.arrival_airport}</p>
-            <p className="text-xs">Airline: {info.flight1.airline}</p>
-          </div>
-        </div>
-        {info.flight2 && (
+    <div className="relative flex">
+      <Card className={cn("w-[400px]", className)} {...props}>
+        <CardHeader>
+          <CardTitle className="flex items-center">
+            {info.flight1.departure_airport} - {info.flight1.arrival_airport}
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-4">
           <div className="border p-4 rounded-md space-y-2">
             <div>
-              <p className="text-sm font-medium">Flight 2</p>
-              <p className="text-xs">Departure Airport: {info.flight2.departure_airport}</p>
-              <p className="text-xs">Arrival Airport: {info.flight2.arrival_airport}</p>
-              <p className="text-xs">Airline: {info.flight2.airline}</p>
+              <p className="text-sm font-medium">Flight 1</p>
+              <p className="text-xs">Departure Airport: {info.flight1.departure_airport}</p>
+              <p className="text-xs">Arrival Airport: {info.flight1.arrival_airport}</p>
+              <p className="text-xs">Airline: {info.flight1.airline}</p>
             </div>
           </div>
-        )}
-        <div>
-          <p className="text-sm font-medium leading-none">Total Cost</p>
-          <p className="text-sm">${info.cost}</p>
-        </div>
-      </CardContent>
-      <CardFooter className="grid gap-4">
-        <Button>
-          <CheckIcon className="mr-2 h-4 w-4" /> Confirm Booking
-        </Button>
-        <Button onClick={openDialog}>
-          <EyeOpenIcon className="mr-2 h-4 w-4" /> View details
-        </Button>
-      </CardFooter>
-      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <DialogContent className="flex flex-col items-center" style={{ minWidth: '800px' }}>
-          <div className="flex space-x-4 mb-4">
-            <FlightCard info={info.flight1} />
-            {info.flight2 && <FlightCard info={info.flight2} />}
+          {info.flight2 && (
+            <div className="border p-4 rounded-md space-y-2">
+              <div>
+                <p className="text-sm font-medium">Flight 2</p>
+                <p className="text-xs">Departure Airport: {info.flight2.departure_airport}</p>
+                <p className="text-xs">Arrival Airport: {info.flight2.arrival_airport}</p>
+                <p className="text-xs">Airline: {info.flight2.airline}</p>
+              </div>
+            </div>
+          )}
+          <div>
+            <p className="text-sm font-medium leading-none">Total Cost</p>
+            <p className="text-sm">${info.cost}</p>
           </div>
-          <DialogClose asChild>
-            <Button>Close</Button>
-          </DialogClose>
-        </DialogContent>
-      </Dialog>
-    </Card>
+        </CardContent>
+        <CardFooter className="grid gap-4">
+          <Button>
+            <CheckIcon className="mr-2 h-4 w-4" /> Confirm Booking
+          </Button>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button>
+                <EyeOpenIcon className="mr-2 h-4 w-4" /> View details
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="w-[90%] sm:w-[80%] lg:w-[25%]" side="right">
+              <SheetHeader>
+                <SheetTitle>Journey Details</SheetTitle>
+                <SheetDescription className="pb-2">
+                  Here are the details of your journey.
+                </SheetDescription>
+              </SheetHeader>
+              <div className="pb-5">
+                <FlightCard info={info.flight1} />
+                {info.flight2 && <FlightCard info={info.flight2} />}
+              </div>
+              <SheetFooter>
+                <SheetClose asChild>
+                  <Button>Close</Button>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
